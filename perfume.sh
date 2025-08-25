44e0e7f579b3b94c2d708488b86c0385429edcd2
kelffren <kelffrencc@gmail.com>
2025-08-25 07:58:43 +0000

    ActualizaciC3n automC!tica

---
+++ perfume.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd pagina_perfumes/Arabic_scentsBronx        # Cambia la ruta a tu repo
+git pull origin main
+git add .
+git commit -m "ActualizaciC3n automC!tica"
+git push origin main
+
